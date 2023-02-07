380989b591895c15e868842852394bb226fb24ee
Hilya Ntinda <113698301+Hilya02@users.noreply.github.com>
2023-02-07 15:32:45 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""
-adds all arguments to a Python list
-"""
-import sys
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+list1 = []
 
-open("add_item.json", "a")
 try:
-    i = load("add_item.json")
-except ValueError:
-    i = []
-save(i + sys.argv[1:], "add_item.json")
+    list1 = list(load_from_json_file("add_item.json"))
+
+except:
+    len(list1) == 0
+
+for arg in range(1, len(argv)):
+    list1.append(argv[arg])
+
+save_to_json_file(list1, "add_item.json")
