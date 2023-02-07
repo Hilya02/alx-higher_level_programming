25f6a5e81a7cd866746c7744643f4f7271a761a3
Hilya Ntinda <113698301+Hilya02@users.noreply.github.com>
2023-02-07 15:38:02 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ adds all arguments to a Python list
 import sys
 
 
-save = __import__('5-save_to_json_file.py').save_to_json_file
-load = __import__('6-load_from_json_file.py').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
 try:
