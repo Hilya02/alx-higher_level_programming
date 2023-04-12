87f616d67a8a5641ed24bacdc7a65333e633f997
Hilya Ntinda <113698301+Hilya02@users.noreply.github.com>
2023-04-12 11:41:22 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(myVar[i]);
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
