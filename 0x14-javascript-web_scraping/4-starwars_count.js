ca79dd5fdc2dacf279e0c59c2b1a5d9678ab350a
Hilya Ntinda Tuyakula <113698301+Hilya02@users.noreply.github.com>
2023-05-23 12:50:14 +0200

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = process.argv[2];
-let times = 0;
+const args = process.argv;
+const requestURL = args[2];
 
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body).results;
-
-  for (let i = 0; i < body.length; ++i) {
-    const characters = body[i].characters;
-
-    for (let j = 0; j < characters.length; ++j) {
-      const character = characters[j];
-      const characterId = character.split('/')[5];
-
-      if (characterId === '18') {
-        times += 1;
+request.get(requestURL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const allData = JSON.parse(body);
+    const results = allData.results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j].includes('18')) { count += 1; }
       }
     }
-  }
 
-  console.log(times);
+    console.log(count);
+  }
 });
